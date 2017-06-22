79529dbe5f39379e4a716f80bad788bb6f46ca36
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2017-06-22 09:42:24 +0300

    Added a skeleton structure for the application startup structure.

--- game.js
+++ game.js
@@ -1,1 +1,30 @@
-// ... acts now as a placeholder ...
+var breakout = (function () {
+
+  function init() {
+    // TODO ...
+  }
+
+  function run() {
+    // TODO ...
+  }
+
+  /**
+   * Start the game.
+   *
+   * Game will be first initialized and then started. Game will be using a
+   * infinite loop (via requestAnimationFrame) as the main loop, so the game
+   * will not stop running until the user closes the browser tab or if an
+   * error is detected by the browser JavaScript engine.
+   */
+  function start() {
+    init();
+    run();
+  }
+
+  return {
+    start: start
+  }
+
+})();
+
+breakout.start();
