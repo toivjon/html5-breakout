0b2805bb2bd24900d5d5929992fff92c67f67bba
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2017-06-22 09:45:44 +0300

    Added missing comments to #init and #run functions.

--- game.js
+++ game.js
@@ -1,9 +1,23 @@
 var breakout = (function () {
 
+  /**
+   * Initialize the game.
+   *
+   * Initialization will ensure that the game will get a reference to the 2D
+   * drawing context from the game canvas element. It also provides a way to
+   * define a game wide initializations for scenes etc.
+   */
   function init() {
     // TODO ...
   }
 
+  /**
+   * Run the game.
+   *
+   * Running the game means that the game will execute an infinite loop that
+   * runs the game logic updates and draw operations until the user closes the
+   * browser tab or the JavaScript catches an exception from the code.
+   */
   function run() {
     // TODO ...
   }
