f9577a130e9cd0073fb057161eb563a7a9f63891
Joni Toiviainen <j.toiviainen@hotmail.co.uk>
2017-06-22 10:00:41 +0300

    Added the logics required to change the active scene.

--- game.js
+++ game.js
@@ -2,6 +2,58 @@ var breakout = (function () {
 
   var canvas;
   var ctx;
+  var scene;
+
+  var welcomeScene = (function () {
+
+    function enter() {
+      // TODO
+    }
+
+    function exit() {
+      // TODO
+    }
+
+    return {
+      enter: enter,
+      exit: exit
+    }
+  })();
+
+  var courtScene = (function () {
+
+    function enter() {
+      // TODO
+    }
+
+    function exit() {
+      // TODO
+    }
+
+  })();
+
+  /**
+   * Set the given scene as the active scene.
+   *
+   * Old active scene (if any) will be first exited by calling the exit
+   * function so it can clean up all necessary resources as required. New
+   * scene will be assigned as the active scene and the enter function will
+   * be called to ensure that the new scene can initialize itself.
+   *
+   * @param {*} newScene A new scene to be applied as the active scene.
+   */
+  function setScene(newScene) {
+    if (newScene) {
+      // perform a cleanup for the old scene (if defined).
+      if (scene) {
+        scene.exit();
+      }
+
+      // apply the new scene and call the scene init (reset).
+      scene = newScene;
+      scene.enter();
+    }
+  }
 
   /**
    * Initialize the game.
@@ -24,7 +76,8 @@ var breakout = (function () {
     // set initial drawing fill color to white.
     ctx.fillStyle = "white";
 
-    // TODO set the initial scene ...
+    // set the welcome scene as the initial scene.
+    setScene(welcomeScene);
   }
 
   /**
